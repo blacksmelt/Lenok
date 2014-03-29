069770484b120d35adab71ff1f26631ccf5473d8
Zhu Ran <blacksmelt@gmail.com>
2014-03-29 16:46:51 +0800

    replace TransformStrategy with Function.

--- src/main/java/cc/zhuran/lambda/TransformStrategy.java
+++
@@ -1,5 +0,0 @@
-package cc.zhuran.lambda;
-
-public interface TransformStrategy<T, R> {
-	public R apply(T object);
-}
--- src/main/java/cc/zhuran/lambda/Transformer.java
+++ src/main/java/cc/zhuran/lambda/Transformer.java
@@ -2,12 +2,13 @@ package cc.zhuran.lambda;
 
 import com.google.common.collect.Lists;
 import java.util.List;
+import java.util.function.Function;
 
 public class Transformer {
-	public static <T, R> List transform(List<T> numbers, TransformStrategy<T, R> transformStrategy) {
+	public static <T, R> List transform(List<T> numbers, Function<T, R> function) {
 		List results = Lists.newArrayList();
 		for (T number : numbers){
-			results.add(transformStrategy.apply(number));
+			results.add(function.apply(number));
 		}
 		return results;
 	}
