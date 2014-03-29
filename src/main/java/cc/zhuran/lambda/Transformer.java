599980317b9ee481378f558b95bcbd853bf85d51
Zhu Ran <blacksmelt@gmail.com>
2014-03-29 17:10:16 +0800

    refactor Transformer.transform method to use stream API.

--- src/main/java/cc/zhuran/lambda/Transformer.java
+++ src/main/java/cc/zhuran/lambda/Transformer.java
@@ -1,15 +1,11 @@
 package cc.zhuran.lambda;
 
-import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Transformer {
-	public static <T, R> List transform(List<T> numbers, Function<T, R> function) {
-		List results = Lists.newArrayList();
-		for (T number : numbers){
-			results.add(function.apply(number));
-		}
-		return results;
+	public static <T, R> List transform(List<T> objects, Function<T, R> function) {
+		return objects.stream().map(function).collect(Collectors.toList());
 	}
 }
